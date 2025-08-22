71e259d99f0c52da2f504aff13b04fa44809aba2
yearshon <yearshon.orrego@inacapmail.cl>
2025-08-22 14:01:15 +0100

    Cambio de unidad 1 de back end

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def inicio(request):
+    contexto = {"nombre": "Yearshon Orrego"}
+    productos = [
+        {"nombre": "Laptop", "precio": 1000},
+        {"nombre": "Celular", "precio": 500},
+        {"nombre": "Tablet", "precio": 300},
+    ]
+    
+    return render(request, "dispositivos/inicio.html", {
+        "contexto": contexto,
+        "productos": productos
+    })
+
+
+def panel_dispositivos(request):
+    dispositivos = [
+        {"nombre": "sensor temperatura", "consumo": 50},
+        {"nombre": "sensor luminosidad", "consumo": 120},
+        {"nombre": "sensor humedad", "consumo": 40},
+        {"nombre": "sensor presion", "consumo": 180},
+    ]
+
+    consumo_maximo = 121
+
+    return render(request, "dispositivos/dispositivos.html", {
+        "dispositivos": dispositivos,
+        "consumo_maximo": consumo_maximo
+    })
