62ab273f0a8dc81fd516ea7fe156a6a0a6201a8d
yearshon <yearshon.orrego@inacapmail.cl>
2025-08-29 13:10:11 +0100

    Se realizaron los cambios jove 29-08-25

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Zona, Dispositivo
+
+admin.site.register([Categoria, Zona])
+
+@admin.register(Dispositivo)
+class DispositivoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'consumo_maximo', 'estado', 'categoria', 'zona')
